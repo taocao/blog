76d5513169514078c86aa7b400f927dd3d1b1975
Tao Cao <aitaocao@gmail.com>
2023-09-13 23:33:55 +0100

    debug

--- generate_index.py
+++ generate_index.py
@@ -1,6 +1,7 @@
 import os
 
 html_files = [f for f in os.listdir('html_output') if f.endswith('.html') and f != 'index.html']
+print("HTML Files found:", html_files)  # Debug line to list the found HTML files
 
 with open('site_template/index.html', 'r') as file:
     data = file.read()
@@ -9,6 +10,8 @@ new_data = data.replace(
     '<!-- Blog links will be inserted here by the script -->',
     '\n'.join([f'<li><a href="{f}">{f}</a></li>' for f in html_files])
 )
+
+print("Generated index content:", new_data)  # Debug line to show the generated index content
 
 with open('html_output/index.html', 'w') as file:
     file.write(new_data)
